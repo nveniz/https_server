f942eb80feb8ac79524800d7f108b477d4db0a21
nveniz <>
2023-04-24 01:24:29 +0300

    Fixed bugs

--- requestQueue.c
+++ requestQueue.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-
-
+#include "types.h"
 
 /*----------Test case -------------
  * This queue has been tested using
@@ -30,19 +23,6 @@
  * has not been tested yet
  *
  */
-typedef struct{
-	struct Node *head;
-	struct Node *tail;
-	pthread_mutex_t mtx;
-	pthread_cond_t cond;
-	SSL_CTX *ctx;
-}REQUESTS;
-
-typedef struct Node{
-	struct Node *next;
-	int socket;
-}NODE;
-
 
 int requests_empty(REQUESTS *q){
 	return (q->head == NULL && q->tail == NULL)?1:0;
@@ -52,14 +32,17 @@ int requests_empty(REQUESTS *q){
 int requests_init(REQUESTS **q, SSL_CTX *ctx){
 	*q = (REQUESTS*)malloc(sizeof(REQUESTS));
 	if(*q == NULL){
+        perror("requests_init: malloc: ");
 		return 1;
 	}
 	(*q)->head = NULL;
 	(*q)->tail = NULL;
 	if(pthread_mutex_init(&(*q)->mtx, NULL) != 0){
+        perror("requests_init: mutex_init: ");
 		return 1;
 	}
 	if(pthread_cond_init(&(*q)->cond,NULL) != 0){
+        perror("requests_init: cond_init: ");
 		return 1;
 	}
 	(*q)->ctx = ctx;
@@ -73,7 +56,8 @@ int requests_add(REQUESTS *q, int client_soc){
 	/* Adding element to queue */
 	NODE *new =(NODE*)malloc(sizeof(NODE));
 	if(new == NULL){
-		return 1;
+        perror("requests_add: malloc: ");
+        exit(EXIT_FAILURE);
 	}
 	new->next = NULL;
 	new->socket = client_soc;
@@ -98,7 +82,7 @@ void requests_get(REQUESTS *q, int *socket){
 	pthread_mutex_lock(&q->mtx);
 
     	/* Wait for element to become available. */
-    	while (queue_empty(q))
+    	while (requests_empty(q))
         	pthread_cond_wait(&q->cond, &q->mtx);
 
     	/* We have an element. Pop it normally and return it in socket. */
@@ -125,58 +109,26 @@ void requests_print(REQUESTS *q){
 	printf("NULL\n");
 }
 
+void requests_cleanup(REQUESTS *q){
 
+    while(q->head != NULL){
+        NODE *tmp = q->head;
+        q->head = q->head->next;
+        free(tmp);
+    }
 
-int thread_serve( REQUESTS *q ){
-
-	int client;
-	SSL *ssl;
-	/* Buf size is 8K */
-	int buf_size = 8192;
-	char *buf[buf_size];
-	
-	while(1){
-
-		requests_get(q, &client);
-
-		/* Creating the TLS handshake with the client fd and SSL_CTX */
-
-		ssl = SSL_new(ctx);
-		SSL_set_fd(ssl, client);
-		
-		/* wait for a TLS/SSL client to initiate a TLS/SSL handshake */
-		if (SSL_accept(ssl) <= 0) {
-			ERR_print_errors_fp(stderr);
-		}
-		/* if TLS/SSL handshake was successfully completed, a TLS/SSL
-		 * connection has been established
-		 */
-
-
-		/* Most http server implementation limit the header size
-		 * to 8K even though there is no limit in the RFC
-		 * 
-		 * The buffer needs to be parsed as a header at first
-		 * in order to find the size of the HTTP body using
-		 * the Content-length in the header*/
-		SSL_read(ssl, buf, buf_size);
-	
-		/* Parse the http request*/
-		//TODO
-		
-		/* Call the neccesary function depending on the request */
-		//TODO
-	
-		
-		/* Managed keep-alive sockets */
-		//TODO
-		
-		/* Close sockets after the HTTP close */
-		//TODO
-	}
+    SSL_CTX_free(q->ctx);
+    if(pthread_mutex_destroy(&q->mtx)){
+        perror("request_cleanup: mutex_destroy: ");
+        exit(EXIT_FAILURE);
+    }   
+    if(pthread_cond_destroy(&q->cond)){
+        perror("request_cleanup: cond_destroy: ");
+        exit(EXIT_FAILURE);
+    }
+    
+    free(q);
 }
-
-
 
 #ifdef DEBUG
 void main(){
